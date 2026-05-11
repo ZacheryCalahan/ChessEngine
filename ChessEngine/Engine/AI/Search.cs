d52db7c69b688ed4f8e17b543bcd0c54a0e2562b
Zachery Calahan <zacherycalahan33@gmail.com>
2026-05-10 23:21:17 -0400

    add: quiescence search

--- ChessEngine/Engine/AI/Search.cs
+++ ChessEngine/Engine/AI/Search.cs
@@ -2,23 +2,26 @@
 
 public class Search
 {
+    Board board;
+    Stopwatch sw;
     const int infinity = 99999999;
+    const int negativeInfinity = -100000000;
+
     public event Action<Move>? OnSearchComplete;
+    volatile bool searchCanceled;
+    bool hasFoundOneMove;
+
     Move bestMoveThisIteration; // This is the best move found within a single iteration
     int bestEvalThisIteration;
-    volatile bool searchCanceled;
     Move bestMove; // This is the best move found within the search
     int bestEval;
-    bool hasFoundOneMove;
-    Board board;
-
-    Stopwatch sw;
+      
 
     public Search(Board board)
     {
         sw = new();
         bestMoveThisIteration = new();
-        bestEvalThisIteration = -infinity;
+        bestEvalThisIteration = negativeInfinity;
         searchCanceled = false;
         hasFoundOneMove = false;
         this.board = board;
@@ -26,50 +29,58 @@ public class Search
 
     public void StartSearchDeepening()
     {
+        string fen = board.ExportFen(); // For board comparison (debugging)
+
+        // Initialize
         searchCanceled = false;
-        hasFoundOneMove = false;
 
         // Iterative deepening search
-        for (int searchDepth = 1; searchDepth < int.MaxValue; searchDepth++)
+        int searchDepth = 1;
+        for (; searchDepth < int.MaxValue; searchDepth++)
         {
+            hasFoundOneMove = false;
+
             // Search through this depth, and set the bestMoveThisIteration
             sw.Restart();
-            SearchMoves(searchDepth, -infinity, infinity);
+            SearchMoves(searchDepth, negativeInfinity, infinity);
             sw.Stop();
 
             if (searchCanceled)
             {
+                // Don't throw away useful results! (i think.)
+                if (hasFoundOneMove)
+                {
+                    bestMove = bestMoveThisIteration;
+                    bestEval = bestEvalThisIteration;
+                }
+
                 Console.WriteLine("Search complete:");
                 Console.WriteLine($"Best eval found: {bestEval}");
                 break;
             }
 
-            Console.WriteLine($"Depth {searchDepth} time : {sw.ElapsedMilliseconds}ms");
-
             // Save this as last iterations best move
             bestMove = bestMoveThisIteration;
             bestEval = bestEvalThisIteration;
             bestMoveThisIteration = new();
-            bestEvalThisIteration = -infinity;
+            bestEvalThisIteration = negativeInfinity;
             hasFoundOneMove = true;
+            Console.WriteLine($"info score cp {bestEval} depth {searchDepth} time {sw.ElapsedMilliseconds} pv {bestMove.ToUCIMoveString()} "); // Report search findings to UCI
         }
 
         // In the case search is canceled before a good move is found
-        if (!hasFoundOneMove)
+        if (bestMove.IsNull)
         {
-            Console.WriteLine("Best move not found!");
+            Console.WriteLine("Best move not found, giving random move.");
+            bestMove = MoveGenerator.GenerateLegalMoves(board)[0]; // Ensure a legal move is returned if best move isn't found.
             return;
         }
 
-        // Sanity
-        var legalMoves = MoveGenerator.GenerateLegalMoves(board);
-        if (!legalMoves.Contains(bestMove))
+        // Ensure board was not malformed
+        if (board.ExportFen() != fen)
         {
-            Console.WriteLine($"Illegal best move selected: {bestMove}");
-            bestMove = legalMoves[0]; // fallback
+            Console.WriteLine("ERROR: Search finished with different board than it started with!");
         }
-
-        OnSearchComplete?.Invoke(bestMove);
 
         OnSearchComplete?.Invoke(bestMove);
     }
@@ -86,43 +97,43 @@ public class Search
             return 0;
         }
 
-        if (depth == 0)
+        if (depth == 0) // Depth remaining
         {
-            return Evaluation.Evaluate(board);
+            // Should do a quiescence search here. (search non-captures)
+            return QuiescenceSearch(alpha, beta);
         }
 
         // Get all available moves and order them
-        List<Move> moves = MoveGenerator.GenerateLegalMoves(board); // Should be possible to use pseudolegal?
+        List<Move> moves = MoveGenerator.GenerateLegalMoves(board);
         MoveOrder.OrderMoves(board, ref moves);
 
-        // If best move exists already, check that move first.
+        // If best move exists already, check that move first. (swap the moves for now?)
         if (hasFoundOneMove && depthFromRoot == 0)
         {
             int idx = moves.IndexOf(bestMove);
             if (idx > 0)
             {
-                moves.RemoveAt(idx);
-                moves.Insert(0, bestMove);
+                Move tmp = moves[0];
+                moves[0] = moves[idx];
+                moves[idx] = tmp;
             }
         }
 
-        // Check if in check or stalemate
+        // Check if in check(mate) or stalemate
         if (moves.Count == 0)
         {
-            // This is temporary!
-            return -10000;
+            if (board.IsInCheck())
+                return -(100000 - depthFromRoot); // Mate score (I do not know why this must be calculated this way.)
+            
+            return 0;
         }
         
-        
-
         // Search moves
         for (int i = 0; i < moves.Count; i++)
         {
             Move move = moves[i];
+
             board.MakeMove(moves[i]);
-
-            // Search can be extended here for interesting cases
-
             int eval = -SearchMoves(depth - 1, -beta, -alpha, depthFromRoot + 1);
             board.UnMakeMove(moves[i]);
 
@@ -132,7 +143,7 @@ public class Search
                 return 0;
             }
 
-            // Stop searching this path if any move would be too good for the opponent
+            // Pruning
             if (eval >= beta)
             {
                 return beta;
@@ -151,7 +162,48 @@ public class Search
                     hasFoundOneMove = true;
                 }
             }
+        }
 
+        return alpha;
+    }
+
+    int QuiescenceSearch(int alpha, int beta)
+    {
+        if (searchCanceled)
+        {
+            return 0;
+        }
+
+        int eval = Evaluation.Evaluate(board); // Captures aren't forced, ensure that bad captures don't overtake good non-captures.
+
+        if (eval >= beta)
+        {
+            return beta;
+        }
+
+        if (eval > alpha)
+        {
+            alpha = eval;
+        }
+
+        List<Move> moves = MoveGenerator.GenerateLegalMoves(board, false, true);
+        MoveOrder.OrderMoves(board, ref moves);
+
+        for (int i = 0; i < moves.Count; i++)
+        {
+            board.MakeMove(moves[i]);
+            eval = -QuiescenceSearch(-beta, -alpha);
+            board.UnMakeMove(moves[i]);
+
+            if (eval >= beta)
+            {
+                return beta;
+            }
+
+            if (eval > alpha)
+            {
+                alpha = eval;
+            }
         }
 
         return alpha;
